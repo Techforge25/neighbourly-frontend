9b5bf7acfd86ca4e60e754c1d3ce13f07f1d05dc
Techforge25 <github@techforgeinnovations.com>
2026-05-08 09:49:56 +0500

    Add Files Changes

--- app/our-story/page.tsx
+++ app/our-story/page.tsx
@@ -1,39 +1,10 @@
 import Banner from "@/components/Banner";
 import Ourstory from "@/components/OurStory/Ourstory";
 import UseFull from "@/components/UseFull";
+import { getMataData } from "@/utils/dumydata";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Suburb says - Our Story",
-  description:"Learn the story behind Suburb says and how we connect you with trusted local services recommended by your neighbors.",
-   alternates: {
-    canonical: "https://www.suburbsays.com.au/our-story",
-  },
-  openGraph: {
-    title: "Suburb says - Our Story",
-    description:"Discover how Suburb says helps you find reliable local services through real neighbor recommendations.",
-    url: "https://www.suburbsays.com.au/our-story",
-    siteName: "Suburb says",
-    
-    images: [
-      {
-        url: "https://www.suburbsays.com.au/images/ourstory.png",
-        width: 1200,
-        height: 630,
-        alt: "Suburb says - Our Story",
-      },
-    ],
-    type: "website",
-  },
-
-  keywords: [
-    "local services",
-    "neighbor recommendations",
-    "trusted services",
-    "community platform",
-    "Suburb says",
-  ],
-};
+export const metadata = getMataData("Suburb says - Our Story");
 
 const page = () => {
   return (
